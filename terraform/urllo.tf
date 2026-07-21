7493064bdd6d5d088a7620ca9eb8b3b15ff0f8a6
Wesley Kirkland <wesley@wesleyk.me>
2026-07-21 15:38:10 -0400

    fix(ci): duplicate checks

--- terraform/urllo.tf
+++ terraform/urllo.tf
@@ -4,9 +4,16 @@ resource "urllo_rule" "example" {
   validate_dns = false
 }
 
+resource "urllo_rule" "example3" {
+  source_urls          = ["go3.unleashthe.cloud"]
+  target_url           = "https://unleashthe.cloud"
+  validate_dns         = true
+  validate_dns_timeout = "1m"
 
-# resource "urllo_rule" "example" {
-#   source_urls  = ["go.unleashthe.cloud"]
-#   target_url   = "https://unleashthe.cloud"
-#   validate_dns = false
-# }
+  response_type = "moved_permanently"
+
+  forward_params = true
+  forward_path   = true
+
+  tags = ["marketing", "migration"]
+}
